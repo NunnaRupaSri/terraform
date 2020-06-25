cd7c99d59412b0ef1fd1cfc2483e1c4c64fbdc87
acer <rupasri797@gmail.com>
2020-06-25 19:18:10 +0530

    add a file

--- instance.tf
+++ instance.tf
@@ -1,6 +1,10 @@
-resource "aws" "aws_instance" {
+provider "aws"  {
   profile = "default"
   region = "us-east-1"
+  ami = " ami-004cd5eb616d96667"
+  instance_type = "t2.micro"
+}
+resource "aws_ami" "aws_instance_type" {
   ami = " ami-004cd5eb616d96667"
   instance_type = "t2.micro"
 }
