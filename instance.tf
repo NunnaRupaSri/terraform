9a69bde8e732dbda30d9e83b7d0ac4ac94e60782
acer <rupasri797@gmail.com>
2020-06-26 16:28:21 +0530

    pushing the changes

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 data "aws_ami" "default" {
   owners = ["973714476881"]
   most_recent = true
@@ -9,27 +10,29 @@ data "aws_ami" "default" {
     values = ["Centos-7-DevOps-Practice"]
   }
 }
-  resource "aws_instance" "web" {
+
+resource "aws_instance" "web" {
   ami = data.aws_ami.default.id
   instance_type = "t2.micro"
   key_name = "Dev-Ops"
 
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
 
-  connection {
+    connection {
     host = self.public_ip
     user = "root"
     password = "DevOps321"
+    }
+    inline = {
+    "yum install nginx",
+    "systemctl start nginx"
+    }
   }
-    inline  {
-    "yum install ngnix"
-    "systemctl start ngnix"
+  tags = {
+  Name = "Terraform-ec2"
   }
 }
-tags = {
-    Name = "Terraform-ec2"
-}
-}
+
 output "PUBLIC_IP_OF_INSTANCE" {
   value = aws_instance.web.public_ip
 }
