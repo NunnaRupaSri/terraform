fcbb94942e86945dfb0e262b55d2a5ba05404915
acer <rupasri797@gmail.com>
2020-06-26 14:28:02 +0530

    add a file

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "web" {
-  ami = "ami-01bd3897dce95258"
+  image = "973714476881"
   instance_type = "t2.micro"
   key_name = "Dev-Ops"
 
