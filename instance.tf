130269b80cbf8b67e6910831389f28a8ac3d1ccb
acer <rupasri797@gmail.com>
2020-06-26 14:10:37 +0530

    add a file

--- instance.tf
+++ instance.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
 }
+resource "aws_instance" "web" {
+  ami = "ami-004cd5eb616d96667"
+  instance_type = "t2.micro"
+  key_name = "Dev-Ops"
 
+tags = {
+    Name = "EC2-Instance" }
+}
+output "PUBLIC_IP_OF_INSTANCE" {
+  value = "aws_instance.web.public.ip"
+}
