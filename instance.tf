2211ced5ea37b63ce8d3485c5c538f0d9451ba45
acer <rupasri797@gmail.com>
2020-06-26 15:10:18 +0530

    add a file

--- instance.tf
+++ instance.tf
@@ -1,8 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "web" {
-  ami = "ami-003ff40010762ace2"
+data "aws_ami" "default" {
+  owners = ["973714476881"]
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["Centos-7-DevOps-Practice"]
+  }
+}
+  resource "aws_instance" "web" {
+  ami = data.aws_ami.default.id
   instance_type = "t2.micro"
   key_name = "Dev-Ops"
 
