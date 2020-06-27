c263e69a1cdbbf8a938f9a5864f1f71880f3840a
acer <rupasri797@gmail.com>
2020-06-27 13:30:04 +0530

    pushing the changes

--- Examples/count.tf
+++ Examples/count.tf
@@ -5,20 +5,9 @@ resource "aws_instance" "web" {
   ami = "ami-003ff40010762ace2"
   instance_type = "t2.micro"
   key_name = "Dev-Ops"
-  provisioner "remote-exec" {
-     connection {
-      host = self.public_ip
-      user = "root"
-      password = "DevOps321"
-    }
-  inline = [
-   "sudo apt-get update",
-    "sudo apt-get install -y php",
-    "sudo apt-get install -y apache2",
-    "sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app"
-  ]
-  }
+  user_data = "sudo service apache2 start"
   tags = {
     Name = "Terraform-ec2"
   }
 }
+
