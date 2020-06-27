94d5899f9885e067cacfa5604801b8134964d292
acer <rupasri797@gmail.com>
2020-06-27 13:14:54 +0530

    pushing the changes

--- Examples/count.tf
+++ Examples/count.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name = "Dev-Ops"
   ssh_username = "centos"
-  type = "amazon-ebs"
+  
   provisioner "local-exec" {
   type = "shell"
   inline = [
