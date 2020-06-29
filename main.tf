2c2556645e1f38f5cca3f8b84eec53fd699045cf
acer <rupasri797@gmail.com>
2020-06-29 15:21:45 +0530

    pushing the changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "web" {
+resource "aws_instance" "example" {
   ami = "ami-003ff40010762ace2"
   instance_type = "t2.micro"
   user_data = <<-EOF
