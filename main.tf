1b0c2635cfcb08ef4eea592401867b79c1bf9bf5
acer <rupasri797@gmail.com>
2020-06-29 15:28:24 +0530

    pushing the changes

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 provider "aws" {
-  region = "us-east-1"
+  region                          = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami = "ami-003ff40010762ace2"
-  instance_type = "t2.micro"
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-  tags = {
-    name = terraform
+  ami                             = "ami-003ff40010762ace2"
+  instance_type                   = "t2.micro"
+  user_data                       = <<-EOF
+                                    #!/bin/bash
+                                    echo "Hello, World" > index.html
+                                    nohup busybox httpd -f -p 8080 &
+                                    EOF
+  tags                            = {
+    name                          = terraform
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-instance"
+  name                            = "terraform"
   ingress {
-    from_port = 8080
-    protocol = "tcp"
-    to_port = 8080
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port                     = 8080
+    protocol                      = "tcp"
+    to_port                       = 8080
+    cidr_blocks                   = ["0.0.0.0/0"]
   }
 
 }
