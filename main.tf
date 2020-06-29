05d5be8b0b3aefd8edd43f69d1c5f9b790a48817
acer <rupasri797@gmail.com>
2020-06-29 16:36:01 +0530

    pushing the changes

--- main.tf
+++ main.tf
@@ -10,13 +10,9 @@ resource "aws_instance" "web" {
   ami                             = "${data.aws_ami.centos7.id}"
   instance_type                   = "t2.micro"
   vpc_security_group_ids          = ["$(aws_security_groups.instance.id)"]
-  user_data                       = <<-EOF
-                                    #!/bin/bash
-                                    echo "Hello, World" > index.html
-                                    nohup busybox httpd -f -p 8080 &
-                                    EOF
+
   tags = {
-    name                          = "example"
+    name                          = "hello world"
   }
 }
 
